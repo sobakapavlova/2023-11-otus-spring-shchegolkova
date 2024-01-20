4f932c9a35d21c2eb7d9ad77f6b60055cf039442
Anastasiya Schegolkova <a.schegolkova@qiwi.com>
2024-01-20 20:57:50 +0300

    fix linter

--- 01-homework/src/main/java/ru/otus/spring/course/service/ConsoleServiceImpl.java
+++ 01-homework/src/main/java/ru/otus/spring/course/service/ConsoleServiceImpl.java
@@ -13,7 +13,7 @@ public class ConsoleServiceImpl implements ConsoleService {
                 question -> {
                     StringBuilder sb = new Formatter()
                             .formatPrint(question.getId(), question.getQuestion(), question.getAnswerList());
-                    outputStream.println(sb);
+                    System.out.println(sb);
                 }
         );
     }
